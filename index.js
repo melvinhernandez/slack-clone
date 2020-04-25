5352fa662dfb15cf5351654dab00abc3ddc23013
Melvin Hernandez <m.hernandez@berkeley.edu>
2020-04-25 13:50:26 -0700

    Setup sequelize models and db

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import typeDefs from './schema';
 import resolvers from './resolvers';
+import models from './models';
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
 server.applyMiddleware({ app });
 
-app.listen({ port: 8080 }, () => console.log(`🚀 Server ready at http://localhost:8080${server.graphqlPath}`));
+models.sequelize.sync().then(() => {
+  app.listen({ port: 8080 }, () => console.log(`🚀 Server ready at http://localhost:8080${server.graphqlPath}`));
+});
---
+++ models/index.js
@@ -0,0 +1,23 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize('slack', 'postgres', 'postgres', {
+  dialect: 'postgres',
+});
+
+const models = {
+  Channel: sequelize.import('./channel'),
+  Message: sequelize.import('./message'),
+  Team: sequelize.import('./team'),
+  User: sequelize.import('./user'),
+};
+
+Object.keys(models).forEach((modelName) => {
+  if ('associate' in models[modelName]) {
+    models[modelName].associate(models);
+  }
+});
+
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
+
+export default models;
