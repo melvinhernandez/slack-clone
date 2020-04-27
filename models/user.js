22e0cd1e2d1d1a711f2faf5afb0f2dabb2911d27
Melvin Hernandez <m.hernandez@berkeley.edu>
2020-04-26 21:40:21 -0700

    Config sequelize underscored setting globally

--- models/user.js
+++ models/user.js
@@ -12,12 +12,15 @@ export default (sequelize, DataTypes) => {
       },
       password: DataTypes.STRING,
     },
-    { underscored: true },
   );
 
   User.associate = (models) => {
     User.belongsToMany(models.Team, {
       through: 'member',
+      foreignKey: { name: 'userId', field: 'user_id' },
+    });
+    User.belongsToMany(models.Channel, {
+      through: 'channel_member',
       foreignKey: { name: 'userId', field: 'user_id' },
     });
   };
