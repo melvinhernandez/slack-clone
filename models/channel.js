ad71e3a715d6c96943e46c52ff0642ee1fddb5be
Melvin Hernandez <m.hernandez@berkeley.edu>
2020-04-26 21:37:42 -0700

    Setup graphql schema

--- models/channel.js
+++ models/channel.js
@@ -12,6 +12,10 @@ export default (sequelize, DataTypes) => {
     Channel.belongsTo(models.Team, {
       foreignKey: { name: 'teamId', field: 'team_id' },
     });
+    Channel.belongsToMany(models.User, {
+      through: 'channel_member',
+      foreignKey: { name: 'channelId', field: 'channel_id' },
+    });
   };
 
   return Channel;
