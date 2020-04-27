5d226eae243a3f4d54edb6694d6010caaaa03bba
Melvin Hernandez <m.hernandez@berkeley.edu>
2020-04-27 00:10:50 -0700

    Setup apollo provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import Routes from './routes';
 
-const App = Routes;
+const client = new ApolloClient({
+  uri: 'http://localhost:8080',
+});
+
+const App = (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
 
 ReactDOM.render(
   <React.StrictMode>
