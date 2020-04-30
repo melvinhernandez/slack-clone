c5c72cae648abe0dd68d63c7a68cb065dc63ad33
Melvin Hernandez <m.hernandez@berkeley.edu>
2020-04-29 21:32:08 -0700

    Add validation fields to user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,27 +1,48 @@
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'user',
-    {
-      username: {
-        type: DataTypes.STRING,
-        unique: true,
+  const User = sequelize.define("user", {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        isAlphanumeric: {
+          args: true,
+          msg: "Username must be alphanumeric.",
+        },
+        len: {
+          args: [3, 25],
+          msg: "Username must be between 3 and 25 characters long.",
+        },
       },
-      email: {
-        type: DataTypes.STRING,
-        unique: true,
-      },
-      password: DataTypes.STRING,
     },
-  );
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: "Invalid email.",
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 100],
+          msg: "Password must be between 3 and 25 characters long.",
+        },
+      },
+    },
+  });
 
   User.associate = (models) => {
     User.belongsToMany(models.Team, {
-      through: 'member',
-      foreignKey: { name: 'userId', field: 'user_id' },
+      through: "member",
+      foreignKey: { name: "userId", field: "user_id" },
     });
     User.belongsToMany(models.Channel, {
-      through: 'channel_member',
-      foreignKey: { name: 'userId', field: 'user_id' },
+      through: "channel_member",
+      foreignKey: { name: "userId", field: "user_id" },
     });
   };
 
